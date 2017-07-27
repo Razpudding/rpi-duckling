b089016c607b4dfc950233a09944081d297a0dfc
Jelle mainPC <Jelle.Aarnoudse@Ziggo.nl>
2017-07-27 15:35:41 +0200

    init verplaatst

--- pwmDC2.py
+++ pwmDC2.py
@@ -14,33 +14,35 @@ class Propulsion(object):
 
     #init the curses screen
     self.stdscr = curses.initscr()
+
     #use cbreak to not require a return key press
     curses.cbreak()
     self.quit=False
 
+    self.speed1 = 30
+    self.speed2 = 30
+
+  def resetGpio(self):
     gpio.setup(18, gpio.OUT)
     gpio.setup(13, gpio.OUT)
     #Set pulse modulation to 50hz for pin 18 and 13 and store that initialization in a va$
-    self.pwm = gpio.PWM(18, 50)
-    self.pwm = gpio.PWM(13, 50)
+    self.pwm1 = gpio.PWM(18, 50)
+    self.pwm2 = gpio.PWM(13, 50)
     #Start the pwm at 0% duty cycle
-    self.pwm.start(0)
-    self.speed = 30
-
-  def resetGpio(self):
-    gpio.setmode(gpio.BCM)
+    self.pwm1.start(0)
+    self.pwm2.start(0)	
     gpio.setup(17, gpio.OUT)
     gpio.setup(22, gpio.OUT)
     gpio.setup(23, gpio.OUT)
     gpio.setup(24, gpio.OUT)
     gpio.setup(18, gpio.OUT)
     gpio.setup(13, gpio.OUT)
-    self.pwm.ChangeDutyCycle(self.speed)
-
+    self.pwm1.ChangeDutyCycle(self.speed1)
+    self.pwm2.ChangeDutyCycle(self.speed2)
+	
   def forward(self, tf):
     self.resetGpio()
-    print("inching forward with speed "+ str(self.speed))
-    self.pwm.ChangeDutyCycle(self.speed)
+    print("inching forward with speed "+ str(self.speed1))
     gpio.output(17, True)
     gpio.output(22, False)
     gpio.output(23, True)
@@ -50,8 +52,7 @@ class Propulsion(object):
 
   def reverse(self, tf):
     self.resetGpio()
-    print("awkwardly backing away with speed "+ str(self.speed))
-    self.pwm.ChangeDutyCycle(self.speed)
+    print("awkwardly backing away with speed "+ str(self.speed1))
     gpio.output(17, False)
     gpio.output(22, True)
     gpio.output(23, False)
@@ -71,10 +72,12 @@ class Propulsion(object):
           self.forward(2)
         elif curses.keyname(c)=="s" :
           self.reverse(2)
-        elif curses.keyname(c)=="u" and self.speed <= 90:
-          self.speed += 10
-        elif curses.keyname(c)=="d" and self.speed >= 40:
-          self.speed -= 10
+        elif curses.keyname(c)=="u" and self.speed1 <= 90:
+          self.speed1 += 10 
+          self.speed2 += 10
+        elif curses.keyname(c)=="d" and self.speed1 >= 40:
+          self.speed1 -= 10
+          self.speed2 -= 10
         #print curses.keyname(c),
         # if curses.keyname(c)=="q" :
         #   quit=True
