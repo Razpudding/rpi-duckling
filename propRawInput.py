7b65a5e7e6a050b608d3b36836d0ef8ed59518a5
Laurens <laurens.aarnoudse@gmail.com>
2017-07-27 15:19:00 +0200

    finally a propulsion system that can start and stop on demand.

--- propRawInput.py
+++ propRawInput.py
@@ -13,19 +13,37 @@ class Propulsion(object):
     gpio.setmode(gpio.BCM)
 
     gpio.setup(18, gpio.OUT)
-    #Set pulse modulation to 50hz for pin 18 and store that initialization in $
-    self.pwm = gpio.PWM(18, 50)
-    #Start the pwm at 0% duty cycle
-    self.pwm.start(30)
-    self.speed = 60
-
-#   gpio.setmode(gpio.BCM)
     gpio.setup(17, gpio.OUT)
     gpio.setup(22, gpio.OUT)
     gpio.setup(23, gpio.OUT)
     gpio.setup(24, gpio.OUT)
 
-  def forward(self, tf):
+    #Set pulse modulation to 50hz for pin 18 and store that initialization in $
+    self.pwm = gpio.PWM(18, 50)
+    #Start the pwm at 0% duty cycle
+    self.pwm.start(0)
+    self.speed = 60
+
+  def forward(self):
+    #self.resetGpio()
+    print("inching forward with speed "+ str(self.speed))
+    self.pwm.ChangeDutyCycle(self.speed)
+    gpio.output(17, True)
+    gpio.output(22, False)
+    gpio.output(23, True)
+    gpio.output(24, False)
+    #time.sleep(tf)
+    #gpio.cleanup()
+
+  def reverse(self, tf):
+    self.resetGpio()
+    #Set pulse modulation to 50hz for pin 18 and store that initialization in $
+    self.pwm = gpio.PWM(18, 50)
+    #Start the pwm at 0% duty cycle
+    self.pwm.start(0)
+    self.speed = 60
+
+  def forward(self):
     #self.resetGpio()
     print("inching forward with speed "+ str(self.speed))
     self.pwm.ChangeDutyCycle(self.speed)
@@ -50,15 +68,35 @@ class Propulsion(object):
   def start(self):
     try:
       while True:
-        print ("waiting for input...")
-        inp= raw_input()
-        if input == "w":
+#        print ("waiting for input...")
+        inp = raw_input()
+        print("input is: " + inp)
+        if inp == "w":
             self.forward()
         #get the entered characters
-        if input == "q":
+        elif inp == "b":
+            self.stop()
+            gpio.output(17, False)
+    gpio.output(22, True)
+    gpio.output(23, False)
+    gpio.output(24, True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+  def start(self):
+    try:
+      while True:
+#        print ("waiting for input...")
+        inp = raw_input()
+        print("input is: " + inp)
+        if inp == "w":
+            self.forward()
+        #get the entered characters
+        elif inp == "b":
             self.stop()
     except KeyboardInterrupt:
-      self.stop()
+      self.quit()
 
   def stop(self):
     print("shutting down all engines")
@@ -66,11 +104,11 @@ class Propulsion(object):
     gpio.output(22, False)
     gpio.output(23, False)
     gpio.output(24, False)
+  def quit(self):
+    print("quitting program")
     self.pwm.ChangeDutyCycle(0)
     gpio.cleanup()
 propulsion = Propulsion()
 propulsion.init()
 propulsion.start()
-
-
 
