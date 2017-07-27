17cbb661c4c273ce27a45adb28ddb0417070849d
Laurens <laurens.aarnoudse@gmail.com>
2017-07-27 16:51:06 +0200

    now featuring left and right

--- continuousMovement.py
+++ continuousMovement.py
@@ -16,30 +16,48 @@ class Propulsion(object):
     gpio.setmode(gpio.BCM)
 
     gpio.setup(18, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
     gpio.setup(17, gpio.OUT)
     gpio.setup(22, gpio.OUT)
     gpio.setup(23, gpio.OUT)
     gpio.setup(24, gpio.OUT)
 
     #Set pulse modulation to 50hz for pin 18 and store that initialization in $
-    self.pwm = gpio.PWM(18, 50)
+    self.pwm1 = gpio.PWM(18, 50)
+    self.pwm2 = gpio.PWM(13, 50)
     #Start the pwm at 0% duty cycle
-    self.pwm.start(0)
-    self.speed = 60
+    self.pwm1.start(0)
+    self.pwm2.start(0)
+    self.speed1 = 60
+    self.speed2 = 60
 
   def forward(self):
-    print("inching forward with speed "+ str(self.speed))
+    print("inching forward with speed "+ str(self.speed1))
     gpio.output(17, True)
     gpio.output(22, False)
     gpio.output(23, True)
     gpio.output(24, False)
 
   def reverse(self):
-    print("awkwardly backing away with speed "+ str(self.speed))
+    print("awkwardly backing away with speed "+ str(self.speed1))
     gpio.output(17, False)
     gpio.output(22, True)
     gpio.output(23, False)
     gpio.output(24, True)
+
+  def left(self):
+    print("to the left ")
+    gpio.output(17, True)
+    gpio.output(22, False)
+    gpio.output(23, False)
+    gpio.output(24, False)
+
+  def right(self):
+    print("to the right ")
+    gpio.output(17, False)
+    gpio.output(22, False)
+    gpio.output(23, True)
+    gpio.output(24, False)
 
   def start(self):
     try:
@@ -56,14 +74,24 @@ class Propulsion(object):
             self.forward()
         elif option == "s":
             self.reverse()
+        elif option == "a":
+            self.left()
+        elif option == "d":
+            self.right()
         elif option == "b":
             self.stop()
-        elif option=="u" and self.speed <= 90:
-          self.speed += 10
-          self.pwm.ChangeDutyCycle(self.speed)
-        elif option=="d" and self.speed >= 40:
-          self.speed -= 10
-          self.pwm.ChangeDutyCycle(self.speed)
+        #Todo check if both speeds are within bounds
+        elif option=="k" and self.speed1 <= 90:
+          self.speed1 += 10
+          self.speed2 += 10
+          self.pwm1.ChangeDutyCycle(self.speed1)
+          self.pwm2.ChangeDutyCycle(self.speed2)
+        elif option=="m" and self.speed1 >= 40:
+          self.speed1 -= 10
+          self.speed2 -= 10
+
+          self.pwm1.ChangeDutyCycle(self.speed1)
+          self.pwm2.ChangeDutyCycle(self.speed2)
     except KeyboardInterrupt:
       self.quit()
 
@@ -76,7 +104,9 @@ class Propulsion(object):
 
   def quit(self):
     print("quitting program")
-    self.pwm.ChangeDutyCycle(0)
+    self.pwm1.ChangeDutyCycle(0)
+    self.pwm2.ChangeDutyCycle(0)
+
     gpio.cleanup()
     curses.endwin()
 propulsion = Propulsion()
