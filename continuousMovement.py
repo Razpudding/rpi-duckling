3ec41e93518c14439cfe170bc394b08da3ae8b60
Laurens <laurens.aarnoudse@gmail.com>
2017-07-28 16:26:11 +0200

    added quickturn functionality

--- continuousMovement.py
+++ continuousMovement.py
@@ -6,7 +6,7 @@ import pygame
 
 class Propulsion(object):
 
-  def init(self):
+  def __init__(self):
     self.stdscr = curses.initscr()
     #I think this line makes sure that if the program crashes, you can still see what you're typing in the console
     curses.endwin()
@@ -28,9 +28,14 @@ class Propulsion(object):
     #Start the pwm at 0% duty cycle
     self.pwm1.start(0)
     self.pwm2.start(0)
-    self.speed1 = 60
-    self.speed2 = 60
-    self.laugh()
+    self.cruisingSpeed = 60
+    self.speedRight = 60
+    self.speedLeft = 60
+    self.state = "awake"
+    # The higher the steerforce, the more extreme the steering effect will be (1/2, 1/3 * power of other wheel)
+    self.steerForce = 4
+    self.quickTurn = True
+    #self.laugh()
 
   def laugh(self):
     pygame.mixer.init()
@@ -39,44 +44,77 @@ class Propulsion(object):
     while pygame.mixer.music.get_busy() == True:
         continue
 
-
   def forward(self):
-    print("inching forward with speed "+ str(self.speed1))
+    print("inching forward with speed "+ str(self.cruisingSpeed))
+    # Reset the speed of both wheels to cruising speed to disable left and right modes
+    self.speedRight = self.speedLeft = self.cruisingSpeed
+    self.updateCycle()
+    self.state = "forward"
     gpio.output(17, True)
     gpio.output(22, False)
     gpio.output(23, True)
     gpio.output(24, False)
 
   def reverse(self):
-    print("awkwardly backing away with speed "+ str(self.speed1))
+    print("awkwardly backing away with speed "+ str(self.cruisingSpeed))
+    self.speedRight = self.speedLeft = self.cruisingSpeed
+    self.updateCycle()
+    self.state = "reverse"
     gpio.output(17, False)
     gpio.output(22, True)
     gpio.output(23, False)
     gpio.output(24, True)
 
   def left(self):
-    print("to the left ")
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, False)
-    gpio.output(24, False)
+    #When driving forward, slow down the left wheel to turn left
+    #As of right now, the state has no 'forward left' soit will just output left
+    if self.state == "forward" and self.speedRight == self.speedLeft:
+        print("forward left")
+        self.speedLeft /= self.steerForce
+        self.updateCycle()
+    # The next mode is for when the bot stands still
+    elif self.state != "forward" and self.state != "reverse":
+        print("to the left from " + self.state)
+        self.speedRight = self.speedLeft = self.cruisingSpeed
+        self.updateCycle()
+        self.state = "left"
+        gpio.output(17, True)
+        gpio.output(22, False)
+        gpio.output(23, False)
+        gpio.output(24, True) if self.quickTurn else gpio.output(24, False)
 
   def right(self):
     print("to the right ")
-    gpio.output(17, False)
-    gpio.output(22, False)
-    gpio.output(23, True)
-    gpio.output(24, False)
+    if self.state == "forward" and self.speedRight == self.speedLeft:
+        print("forward right")
+        self.speedRight /= self.steerForce
+        self.updateCycle()
+        #TODO: updte state here as well otherwise you cant do a quickturn after forward left/right
+    # The next mode is for when the bot stands still
+    elif self.state != "forward" and self.state != "reverse":
+        print("to the right from " + self.state)
+        #reset the speed of both wheels before turning.
+        self.speedRight = self.speedLeft = self.cruisingSpeed
+        self.updateCycle()
+        self.state = "right"
+        gpio.output(17, False)
+        # This is a nifty trick to control quickturning. If quickturn is true, the left wheel turns backwards,
+        # the right wheel forward; if quickturn is fals, the left wheel stops (false, false) the right wheel turns forward.
+        gpio.output(22, True) if self.quickTurn else gpio.output(22, False)
+        gpio.output(23, True)
+        gpio.output(24, False)
+
+  def goDuckYour(self):
+    print("Loose yourself (autonomous mode)") 
+
 
   def start(self):
     try:
       while True:
-#        print ("waiting for input...")
-        #inp = raw_input()
         curses.cbreak()
         userInput = self.stdscr.getch()
         option = curses.keyname(userInput)
-        #dont know what this line does tbh
+        #The next line makes sure your terminal typing wont crash
         curses.endwin()
         print("input is: " + str(option))
         if option == "w":
@@ -90,27 +128,39 @@ class Propulsion(object):
         elif option == "b":
             self.stop()
         #Todo check if both speeds are within bounds
-        elif option=="k" and self.speed1 <= 90:
-          self.speed1 += 10
-          self.speed2 += 10
-          self.pwm1.ChangeDutyCycle(self.speed1)
-          self.pwm2.ChangeDutyCycle(self.speed2)
-        elif option=="m" and self.speed1 >= 40:
-          self.speed1 -= 10
-          self.speed2 -= 10
-
-          self.pwm1.ChangeDutyCycle(self.speed1)
-          self.pwm2.ChangeDutyCycle(self.speed2)
+        elif option=="k" and self.cruisingSpeed <= 90:
+          self.cruisingSpeed += 10
+          self.speedRight *= 1.1
+          self.speedLeft *= 1.1
+          self.updateCycle()
+          # self.pwm1.ChangeDutyCycle(self.speedRight * 1.1)
+          #self.pwm2.ChangeDutyCycle(self.speedLeft * 1.1)
+        elif option=="m" and self.cruisingSpeed >= 40:
+          self.cruisingSpeed -= 10
+          self.speedRight *= .9
+          self.speedLeft *= .9
+          self.updateCycle()
+        #build in a bit of a delay to not overwork the processor too much
+        #controls should still be snappy at this speed
         time.sleep(.1)
     except KeyboardInterrupt:
       self.quit()
 
   def stop(self):
-    print("shutting down all engines")
+    print("Stop! Hammertime")
+    self.state = "still"
     gpio.output(17, False)
     gpio.output(22, False)
     gpio.output(23, False)
     gpio.output(24, False)
+
+  def updateCycle(self):
+    self.pwm1.ChangeDutyCycle(self.speedRight)
+    self.pwm2.ChangeDutyCycle(self.speedLeft)
+    # if wheel == "left":
+    #     self.pwm2.ChangeDutyCycle(speed)
+    # elif wheel == "right":
+    #     self.pwm1.ChangeDutyCycle(speed)
 
   def quit(self):
     print("quitting program")
@@ -120,6 +170,6 @@ class Propulsion(object):
     gpio.cleanup()
     curses.endwin()
 propulsion = Propulsion()
-propulsion.init()
+#propulsion.init()
 propulsion.start()
 
