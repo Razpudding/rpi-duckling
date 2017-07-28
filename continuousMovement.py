97b43923b7623f8df31a0b4506e9386665332aa4
Laurens <laurens.aarnoudse@gmail.com>
2017-07-29 00:17:33 +0200

    Added autonomous behaviour. 2 modes for now. Also rewrote steering mechanism

--- continuousMovement.py
+++ continuousMovement.py
@@ -3,18 +3,18 @@ import time
 #curses is used to capture the keyboard input
 import curses
 import pygame
+from random import randint
 
+#TODO: re-enable pylinter and set it up properly
+#TODO: set up a finally that cleans up the GPIOs always instead of just after a keyboardbreak (ctr-c)
 class Propulsion(object):
-
   def __init__(self):
     self.stdscr = curses.initscr()
-    #I think this line makes sure that if the program crashes, you can still see what you're typing in the console
     curses.endwin()
+    #Disable warnings for the GPIOs being in use
     gpio.setwarnings(False)
-
     #Set the pinmode to BCM
     gpio.setmode(gpio.BCM)
-
     gpio.setup(18, gpio.OUT)
     gpio.setup(13, gpio.OUT)
     gpio.setup(17, gpio.OUT)
@@ -22,15 +22,17 @@ class Propulsion(object):
     gpio.setup(23, gpio.OUT)
     gpio.setup(24, gpio.OUT)
 
-    #Set pulse modulation to 50hz for pin 18 and store that initialization in $
+    #Set pulse modulation to 50hz for pin 18 and store that initialization in a variable
     self.pwm1 = gpio.PWM(18, 50)
     self.pwm2 = gpio.PWM(13, 50)
     #Start the pwm at 0% duty cycle
     self.pwm1.start(0)
     self.pwm2.start(0)
+    #The normal speed for forwards and backwards momentum
     self.cruisingSpeed = 60
     self.speedRight = 60
     self.speedLeft = 60
+    #Initialize the state of the propulsion
     self.state = "awake"
     # The higher the steerforce, the more extreme the steering effect will be (1/2, 1/3 * power of other wheel)
     self.steerForce = 4
@@ -47,6 +49,7 @@ class Propulsion(object):
   def forward(self):
     print("inching forward with speed "+ str(self.cruisingSpeed))
     # Reset the speed of both wheels to cruising speed to disable left and right modes
+    #TODO: put the following 2 lines in a function so the code is more DRY
     self.speedRight = self.speedLeft = self.cruisingSpeed
     self.updateCycle()
     self.state = "forward"
@@ -87,6 +90,8 @@ class Propulsion(object):
     print("to the right ")
     if self.state == "forward" and self.state != "right":
         print("forward right")
+        #Todo, move speed to the turnWheel function as well, makes for easier code when turning when reversing
+        #Maybe its best to turn wheels into an object and have the rest of the script only rack if the bot is moving forwards or backwards
         self.speedRight = self.speedLeft = self.cruisingSpeed
         self.speedRight /= self.steerForce
         self.updateCycle()
@@ -97,14 +102,55 @@ class Propulsion(object):
         self.speedRight = self.speedLeft = self.cruisingSpeed
         self.updateCycle()
         self.state = "right"
-        # This is a nifty trick to control quickturning. If quickturn is true, the left wheel turns backward,
-        # the right wheel forward; if quickturn is false, the left wheel stops (false, false) the right wheel turns forward.
         self.turnWheel("right", "reverse") if self.quickTurn else self.turnWheel("right", "stop")
         self.turnWheel("left", "forward")
 
   def goDuckYour(self):
+    '''This function selects a random sequence and executes it. 
+    Each sequence contains 1 or more instructions like "left" or "forward. Each instruction is coupled to a function
+    each function sets the GPIOs up. After each instruction the sequence will wait for a number of seconds.
+    The coding in this function may look a bit ugly that's what you get with custom sequences...'''
     print("Loose yourself (autonomous mode)") 
+    testMoves = [self.left, self.right, self.reverse]
+    waggleMoves = [self.left, self.right]
+    sequence2 = ["enableQuickTurn"]
+    waggleMode = True
 
+    def test():
+        ''''This mode is for testing out fun sequences. Just pop em in testmoves and watch her go.
+        If you find a nice combo, save it an array, give it a name in the modemapping object and gooo'''
+        print("Executing testmoves")
+        for instruction in testMoves:
+            instruction()
+            time.sleep(5)
+
+    def waggle():
+        '''Waggle works like this: left,right,left,random quickturn, repeat patter with %chance through recursion'''
+        print("Executing waggle")
+        self.enableQuickTurn(False)
+        for i in range(3):
+            waggleMoves[0]()
+            time.sleep(.5)
+            waggleMoves[1]()
+            time.sleep(.5)
+        self.enableQuickTurn(True)
+        waggleMoves[randint(0,1)]()
+        time.sleep(.4)
+        if (randint(0,9) >= 4):
+            waggle()
+    #TODO: find a way to interrupt sleep so this process can be stopped by the user
+    #This dict holds the dirrent modes for autonomous movement
+    modeMapping = {
+        "1" : test,
+        "2" : waggle,
+        "3" : "spin"
+    }
+    userInput = self.stdscr.getch()
+    option = curses.keyname(userInput)
+    curses.endwin()
+    modeMapping[option]()
+    
+    self.stop()
 
   def start(self):
     try:
@@ -112,9 +158,9 @@ class Propulsion(object):
         curses.cbreak()
         userInput = self.stdscr.getch()
         option = curses.keyname(userInput)
-        #The next line makes sure your terminal typing wont crash
         curses.endwin()
         print("input is: " + str(option))
+        #we could convert this to a dictionary mapping but that wouldnt work for the more complex options
         if option == "w":
             self.forward()
         elif option == "s":
@@ -125,21 +171,20 @@ class Propulsion(object):
             self.right()
         elif option == "b":
             self.stop()
+        elif option == "0":
+            self.goDuckYour()
         #Todo check if both speeds are within bounds
         elif option=="k" and self.cruisingSpeed <= 90:
           self.cruisingSpeed += 10
           self.speedRight *= 1.1
           self.speedLeft *= 1.1
           self.updateCycle()
-          # self.pwm1.ChangeDutyCycle(self.speedRight * 1.1)
-          #self.pwm2.ChangeDutyCycle(self.speedLeft * 1.1)
         elif option=="m" and self.cruisingSpeed >= 40:
           self.cruisingSpeed -= 10
           self.speedRight *= .9
           self.speedLeft *= .9
           self.updateCycle()
         #build in a bit of a delay to not overwork the processor too much
-        #controls should still be snappy at this speed
         time.sleep(.1)
     except KeyboardInterrupt:
       self.quit()
@@ -171,6 +216,9 @@ class Propulsion(object):
         elif direction == "stop":
             gpio.output(17, False)
             gpio.output(22, False)
+
+  def enableQuickTurn(self, value):
+    self.quickTurn = value
 
   def updateCycle(self):
     self.pwm1.ChangeDutyCycle(self.speedRight)
